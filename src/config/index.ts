8b056f31cdab89f06b18ed05165e98b6ed90661a
Edidiong Ndaobong <100199901+EddieBenn@users.noreply.github.com>
2026-03-31 15:39:52 +0100

    project setup completed

---
+++ src/config/index.ts
@@ -0,0 +1,18 @@
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+export const config = {
+  DEFAULT_PER_PAGE: 10,
+  DEFAULT_PAGE_NO: 1,
+  PORT: Number(process.env.PORT) || 3000,
+  APP_URL: process.env.APP_URL || 'http://localhost:',
+  NODE_ENV: process.env.NODE_ENV || 'development',
+  DB_PORT: Number(process.env.DB_PORT),
+  DB_DATABASE: process.env.DB_DATABASE,
+  DB_HOST: process.env.DB_HOST,
+  DB_PASSWORD: process.env.DB_PASSWORD,
+  DB_USERNAME: process.env.DB_USERNAME,
+  JWT_SECRET: process.env.JWT_SECRET,
+  JWT_EXPIRY: process.env.JWT_EXPIRY,
+};
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,55 @@
-import { NestFactory } from '@nestjs/core';
+import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {
+  HttpStatus,
+  UnprocessableEntityException,
+  ValidationPipe,
+} from '@nestjs/common';
+import { HttpExceptionFilter } from './filters/exception-filter';
+import { initializeTransactionalContext } from 'typeorm-transactional';
+import {
+  ExpressAdapter,
+  type NestExpressApplication,
+} from '@nestjs/platform-express';
+import rateLimit from 'express-rate-limit';
+import cookieParser from 'cookie-parser';
+import { config } from './config';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+async function bootstrap(): Promise<NestExpressApplication> {
+  initializeTransactionalContext();
+  const app = await NestFactory.create<NestExpressApplication>(
+    AppModule,
+    new ExpressAdapter(),
+    { cors: true },
+  );
+
+  const reflector = app.get(Reflector);
+
+  const PORT = config.PORT;
+
+  app.use(
+    rateLimit({
+      windowMs: 15 * 60 * 1000, // 15 minutes
+      max: 100, // limit each IP to 100 requests per 15 mintutes
+    }),
+  );
+  app.use(cookieParser());
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      errorHttpStatusCode: HttpStatus.UNPROCESSABLE_ENTITY,
+      exceptionFactory: (errors) => new UnprocessableEntityException(errors),
+    }),
+  );
+
+  app.useGlobalFilters(new HttpExceptionFilter(reflector));
+
+  await app.listen(PORT, () => {
+    console.log(`🚀 Server running on port:: ${PORT}`);
+  });
+
+  return app;
 }
-bootstrap();
+void bootstrap();
